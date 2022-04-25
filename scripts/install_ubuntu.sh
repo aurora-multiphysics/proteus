4a655e0d17dfdca006cb58501dfeb59a00e2a565
Aleksander J. Dubas <aleksander.dubas@ukaea.uk>
2022-04-25 15:13:46 +0100

    Add script for installation on Ubuntu.  WIP.

---
+++ scripts/install_ubuntu.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# This script installs Proteus on Ubuntu,
+# including a MOOSE framework build in the $HOME directory.
+# Optimised to the native system architecture.
+# A .moose_profile script is added to the $HOME directory.
+# Use the installation by typing:
+#   source $HOME/.moose_profile
+
+export PROTEUS_DIR=`pwd`
+
+# Make MOOSE profile
+
+echo "export CC=mpicc" >> $HOME/.moose_profile
+echo "export CXX=mpicxx" >> $HOME/.moose_profile
+echo "export F90=mpif90" >> $HOME/.moose_profile
+echo "export F77=mpif77" >> $HOME/.moose_profile
+echo "export FC=mpif90" >> $HOME/.moose_profile
+echo "export MOOSE_DIR="$HOME"/moose" >> $HOME/.moose_profile
+source $HOME/.moose_profile
+
+# Clone MOOSE from git
+
+cd $HOME
+git clone https://github.com/idaholab/moose.git
+
+# Build PETSc
+
+cd $MOOSE_DIR
+unset PETSC_DIR PETSC_ARCH
+./scripts/update_and_rebuild_petsc.sh \
+CC=$CC CXX=$CXX F90=$F90 F77=$F77 FC=$FC \
+--CXXOPTFLAGS="-O3 -march=native" \
+--COPTFLAGS="-O3 -march=native" \
+--FOPTFLAGS="-O3 -march=native" \
+--download-mumps=0
+
+# Build libMesh
+
+./scripts/update_and_rebuild_libmesh.sh --with-mpi
+
+# Configure AD
+
+./configure --with-derivative-size=200 --with-ad-indexing-type=global
+
+cd $PROTEUS_DIR
+make
+
+echo "Installation complete."
