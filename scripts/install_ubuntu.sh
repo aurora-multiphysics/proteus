e1f86b50add626502043773848abbeb48d379b1b
Aleksander J. Dubas <aleksander.dubas@ukaea.uk>
2022-06-07 12:52:27 +0100

    Fix install_ubuntu script for 20.04 LTS.

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -4,19 +4,28 @@
 # including a MOOSE framework build in the $HOME directory.
 # Optimised to the native system architecture.
 # A .moose_profile script is added to the $HOME directory.
+# This script is intended to be used from the proteus directory
+#   ./scripts/intall_ubuntu.sh
 # Use the installation by typing:
 #   source $HOME/.moose_profile
 
 export PROTEUS_DIR=`pwd`
 
+# Install pre-requisites
+
+sudo apt install gcc cmake bison flex git
+sudo apt install python3 python3-dev python-is-python3
+sudo apt install openmpi-bin libopenmpi-dev
+
 # Make MOOSE profile
 
-echo "export CC=mpicc" >> $HOME/.moose_profile
+echo "export CC=mpicc" > $HOME/.moose_profile
 echo "export CXX=mpicxx" >> $HOME/.moose_profile
 echo "export F90=mpif90" >> $HOME/.moose_profile
 echo "export F77=mpif77" >> $HOME/.moose_profile
 echo "export FC=mpif90" >> $HOME/.moose_profile
 echo "export MOOSE_DIR="$HOME"/moose" >> $HOME/.moose_profile
+echo "export PATH=$PATH:"$PROTEUS_DIR >> $HOME/.moose_profile
 source $HOME/.moose_profile
 
 # Clone MOOSE from git
@@ -33,7 +42,7 @@ CC=$CC CXX=$CXX F90=$F90 F77=$F77 FC=$FC \
 --CXXOPTFLAGS="-O3 -march=native" \
 --COPTFLAGS="-O3 -march=native" \
 --FOPTFLAGS="-O3 -march=native" \
---download-mumps=0
+--download-mumps=0 --download-superlu_dist=0
 
 # Build libMesh
 
