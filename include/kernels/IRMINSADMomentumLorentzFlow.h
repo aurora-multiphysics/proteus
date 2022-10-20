8b2c1511f3979179abb749a9fe354a544ff662e7
Rupert Eardley <rupert.eardley@ukaea.uk>
2022-10-20 16:40:49 +0100

    WIP: Initial attempt at Lorentz force sigma(UxB)xB term, does not converge

---
+++ include/kernels/IRMINSADMomentumLorentzFlow.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ADKernelValue.h"
+
+// class IRMINSADMomentumLorentzFlow : public ADKernelValue
+class IRMINSADMomentumLorentzFlow : public ADVectorKernelValue
+{
+public:
+  static InputParameters validParams();
+
+  IRMINSADMomentumLorentzFlow(const InputParameters & parameters);
+
+protected:
+  virtual ADRealVectorValue precomputeQpResidual() override;
+
+  const ADVectorVariableValue & _velocity;
+  const ADVectorVariableValue & _magnetic_field;
+
+  const ADMaterialProperty<Real> & _conductivity;
+};
---
+++ src/kernels/IRMINSADMomentumLorentzFlow.C
@@ -0,0 +1,35 @@
+#include "IRMINSADMomentumLorentzFlow.h"
+
+registerMooseObject("ProteusApp", IRMINSADMomentumLorentzFlow);
+
+InputParameters
+IRMINSADMomentumLorentzFlow::validParams()
+{
+  InputParameters params = ADKernelValue::validParams();
+  params.addClassDescription(
+      "The Lorentz force velocity term ($\\sigma (\\vec{u} \\times \\vec{B}_0) \\times \\vec{B}_0), "
+      "with the weak form of $(\\phi_i, \\sigma (\\vec{u} \\times \\vec{B}_0) \\times \\vec{B}_0). "
+      "The Jacobian is computed using automatic differentiation");
+  params.addRequiredCoupledVar("velocity", "The variable representing the velocity.");
+  params.addRequiredCoupledVar("magneticField", "The variable representing the magnetic field.");
+  params.addParam<MaterialPropertyName>("conductivity", "conductivity", "The name of the conductivity");
+
+  return params;
+}
+
+IRMINSADMomentumLorentzFlow::IRMINSADMomentumLorentzFlow(const InputParameters & parameters)
+  // : ADKernelValue(parameters),
+  : ADVectorKernelValue(parameters),
+  _velocity(adCoupledVectorValue("velocity")),
+  _magnetic_field(adCoupledVectorValue("magneticField")),
+  _conductivity(getADMaterialProperty<Real>("conductivity"))
+{
+}
+
+ADRealVectorValue
+IRMINSADMomentumLorentzFlow::precomputeQpResidual()
+{
+  // auto UxB = _velocity[_qp].cross(_magnetic_field[_qp]);
+  // return _conductivity[_qp] * UxB.cross(_magnetic_field[_qp]);
+  return -_magnetic_field[_qp];
+}
