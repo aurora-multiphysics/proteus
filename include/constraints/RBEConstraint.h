23c2b474532862b336ca910de8fd428f31cba39b
Aleksander J. Dubas <aleksander.dubas@ukaea.uk>
2024-11-20 15:00:59 +0000

    Add initial implementation of RBE Constraint.

---
+++ include/constraints/RBEConstraint.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "NodalConstraint.h"
+
+class RBEConstraint : public NodalConstraint
+{
+  public:
+    static InputParameters validParams();
+    RBEConstraint(const InputParameters & parameters);
+
+  protected:
+    virtual Real computeQpResidual(Moose::ConstraintType type) override;
+    virtual Real computeQpJacobian(Moose::ConstraintJacobianType type) override;
+    std::string _primary_node_set_id;
+    std::string _secondary_node_set_id;
+    Real _penalty;
+    Real _primary_size;
+};
