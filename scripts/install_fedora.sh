bb15827279ea35bee3fea5f67047e3b9a5c488a0
Aleksander J. Dubas <aleksander.dubas@ukaea.uk>
2025-07-24 16:08:48 +0100

    Add Fedora install script, WIP.

---
+++ scripts/install_fedora.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# This script installs Proteus on Fedora,
+# including a MOOSE framework build in the $HOME directory.
+# Optimised to the native system architecture.
+# A .proteus_profile script is added to the $HOME directory.
+# This script is intended to be used from the proteus directory
+#   ./scripts/install_fedora.sh
+# Use the installation by typing:
+#   source $HOME/.proteus_profile
+
+export PROTEUS_DIR=`pwd`
+
+# If MOOSE_JOBS is unset, set to 1
+if [ -z $MOOSE_JOBS ]; then
+  export MOOSE_JOBS=1
+fi
+export METHODS="opt"
+
+# Make Proteus profile
+
+echo "export CC=mpicc" > $HOME/.proteus_profile
+echo "export CXX=mpicxx" >> $HOME/.proteus_profile
+echo "export F90=mpif90" >> $HOME/.proteus_profile
+echo "export F77=mpif77" >> $HOME/.proteus_profile
+echo "export FC=mpif90" >> $HOME/.proteus_profile
+echo "export MOOSE_DIR="$HOME"/moose" >> $HOME/.proteus_profile
+echo "export PATH=\$PATH:"$PROTEUS_DIR >> $HOME/.proteus_profile
+source $HOME/.proteus_profile
+
+# Clone MOOSE from git
+
+cd $HOME
+git clone https://github.com/idaholab/moose.git
+
+# Build PETSc
+
+cd $MOOSE_DIR
+unset PETSC_DIR PETSC_ARCH
+./scripts/update_and_rebuild_petsc.sh \
+--CXXOPTFLAGS="-O3 -march=native" \
+--COPTFLAGS="-O3 -march=native" \
+--FOPTFLAGS="-O3 -march=native" \
+--without-ptscotch --download-ptscotch=0 \
+--without-scalapack --download-scalapack=0 \
+--without-strumpack --download-strumpack=0 \
+--without-mumps --download-mumps=0 \
+--without-superlu_dist --download-super_dist=0 | tee $PROTEUS_DIR/log.petsc_build
+
+# Build libMesh
+
+./scripts/update_and_rebuild_libmesh.sh --with-mpi | tee $PROTEUS_DIR/log.libmesh_build
+
+# Build WASP
+
+./scripts/update_and_rebuild_wasp.sh | tee $PROTEUS_DIR/log.wasp_build
+
+# Configure AD
+# Derivative size should be the total of
+# 8 for each first order variable
+# 27 for each second order variable
+
+./configure --with-derivative-size=89 | tee $PROTEUS_DIR/log.moose_configure
+
+cd $PROTEUS_DIR
+make -j $MOOSE_JOBS | tee $PROTEUS_DIR/log.proteus_build
+
+echo "Installation complete."
