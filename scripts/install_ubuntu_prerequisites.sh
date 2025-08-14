332e89f0019c6672c0b7b75dbaa856955b3f07bb
Aleksander J. Dubas <aleksander.dubas@ukaea.uk>
2025-08-14 02:22:06 +0100

    Add numpy, pandas, pyaml and jinja2 dependencies to Ubuntu prereq.

--- scripts/install_ubuntu_prerequisites.sh
+++ scripts/install_ubuntu_prerequisites.sh
@@ -8,4 +8,6 @@
 
 sudo apt install -y gcc g++ gfortran cmake bison flex git
 sudo apt install -y python3 python3-dev python-is-python3 python3-packaging
+sudo apt install -y python3-numpy python3-pandas
+sudo apt install -y python3-pretty-yaml python3-jinja2
 sudo apt install -y openmpi-bin libopenmpi-dev libboost-all-dev libtirpc-dev
